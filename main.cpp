8a10f9021a3289399ffff4b29889a368976a4501
Anish kumar dubey <anishkumardubey@Anishs-MacBook-Pro-8.local>
2025-04-20 17:01:06 +0530

    init

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
+#include "raylib.h"
+
+Color green = {173, 204, 96, 255};
+Color darkGreen = {43, 51, 24, 255};
+
 int main(){
-    
+    InitWindow(750, 750, "Retro Snake");
+    SetTargetFPS(60);
+
+    while(WindowShouldClose() == false){
+        BeginDrawing();
+
+        ClearBackground(green);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
